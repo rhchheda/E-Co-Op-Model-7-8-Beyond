f302c3a679560a78d2abaf667a6fb900ecc85613
rhchheda <rakesh.chheda@kletech.ac.in>
2026-07-06 16:25:39 +0530

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-// Fill this in after creating your Google Sheet (see apps-script/Setup.gs and README.md).
-// It's the long ID in the sheet's URL:
-// https://docs.google.com/spreadsheets/d/1oQGtAUjOEk1K1NhwN0rXJrdUOyiti7z-yW9qtjYw5ZA/edit
-const CONFIG = {
-  SHEET_ID: "1oQGtAUjOEk1K1NhwN0rXJrdUOyiti7z-yW9qtjYw5ZA",
-};
